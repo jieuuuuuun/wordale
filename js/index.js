deb47890fb17b04f880648741cdffc03c339031a
jieuuuuuun <qkdwldms12@naver.com>
2024-01-17 22:44:26 +0900

    [워들] javascript로 워들 만들기
    워들 만들었어요

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
-/*1. 5글자 단어 (존재하는 단어가 아니어도 됨)
+/*
+1. 5글자 단어 (존재하는 단어가 아니어도 됨)
 2. 6번의 시도 가능
 3. 존재하면 노란색, 위치도 맞으면 초록색으로 표시
 4. 게임 종료 판단
-5. 상단에 게임 시간 표시하기 */
+5. 상단에 게임 시간 표시하기
+선택 키보드에도 동일하게 표시
+키보드 클릭으로도 입력 
+*/
+
 const 정답 = "APPLE";
 
 let attempts = 0;
 let index = 0;
+let timer;
 
 function appStart() {
+  //게임종료 함수
   const displayGameover = () => {
     const div = document.createElement("div");
     div.innerText = "게임이 종료됐습니다.";
@@ -16,18 +23,20 @@ function appStart() {
       "display:flex; justify-content:center; align-items:center; position:absolute; top:40vh;left:45vw;background-color:#fff;width:200px";
     document.body.appendChild(div);
   };
-
   const gameover = () => {
     window.removeEventListener("keydown", handleKeydown);
     displayGameover();
+    clearInterval(timer);
   };
 
+  //다음줄로 이동 함수
   const nextLine = () => {
     if (attempts === 6) return gameover();
     attempts += 1;
     index = 0;
   };
 
+  //엔터키 눌렀을 때 함수
   const handlEnterKey = () => {
     let 맞은_갯수 = 0;
     for (let i = 0; i < 5; i++) {
@@ -46,7 +55,19 @@ function appStart() {
     if (맞은_갯수 === 5) gameover();
     else nextLine();
   };
-  const handleBackspace = () => {};
+
+  //백스페이스 클릭시
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  //키보드 눌렀을 때 함수
   const handleKeydown = (event) => {
     const key = event.key.toUpperCase();
     const keyCode = event.keyCode;
@@ -55,8 +76,7 @@ function appStart() {
     );
 
     if (event.key === "Backspace") handleBackspace();
-
-    if (index === 5) {
+    else if (index === 5) {
       if (event.key === "Enter") handlEnterKey();
       else return;
     } else if (65 <= keyCode && keyCode <= 90) {
@@ -64,6 +84,21 @@ function appStart() {
       index += 1;
     }
   };
+  //타이머
+  function startTimer() {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+    timer = setInterval(setTime, 1000);
+  }
+  startTimer();
   window.addEventListener("keydown", handleKeydown);
 }
 
